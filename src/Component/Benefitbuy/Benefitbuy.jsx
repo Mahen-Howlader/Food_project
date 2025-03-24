e560b4c5d2fe97300ce18d93e8cac4c8aaebad4a
devmahin <mahinhowlader123@gmail.com>
2025-03-24 23:14:37 +0530

    new hero section add

--- src/Component/Benefitbuy/Benefitbuy.jsx
+++ src/Component/Benefitbuy/Benefitbuy.jsx
@@ -10,23 +10,23 @@ function Benefitbuy(props) {
             <div className='mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
                 <div className='bg-gray-700 p-4 r flex rounded-md  flex-col justify-center items-center text-center space-y-1 ounded-md text-white'>
                     <FaCarSide className='' size={35} />
-                    <h2 className='text-3xl'>Fast Delivery</h2>
-                    <p className="text-gray-300 text-xl">Same day delivery as soon as possible</p>
+                    <h2 className='text-2xl font-bold shadow-xl'>Fast Delivery</h2>
+                    <p className="text-gray-300 text-sm">Same day delivery as soon as possible</p>
                 </div>
                 <div className='bg-gray-700 p-4 ro flex rounded-md  flex-col justify-center items-center text-center space-y-1 unded-md text-white'>
                     <MdDiscount className='' size={35} />
-                    <h2 className='text-3xl'>Special Discount</h2>
-                    <p className="text-gray-300 text-xl">Get attractive offers day by day</p>
+                    <h2 className='text-2xl font-bold shadow-xl'>Special Discount</h2>
+                    <p className="text-gray-300 text-sm">Get attractive offers day by day</p>
                 </div>
                 <div className='bg-gray-700 p-4 ro flex rounded-md  flex-col justify-center items-center text-center space-y-1 unded-md text-white'>
                     <GrInsecure className='' size={35} />
-                    <h2 className='text-3xl'>Secure Checkout</h2>
-                    <p className="text-gray-300 text-xl">Fully secured SSL checkout technology</p>
+                    <h2 className='text-2xl font-bold shadow-xl'>Secure Checkout</h2>
+                    <p className="text-gray-300 text-sm">Fully secured SSL checkout technology</p>
                 </div>
                 <div className='bg-gray-700 p-4 rounded-md flex  flex-col justify-center items-center text-center space-y-1  text-white'>
                     <FaMoneyBillTrendUp className='' size={35} />
-                    <h2 className='text-3xl'>Money Returns</h2>
-                    <p className="text-gray-300 text-xl">100% Money back guarntee</p>
+                    <h2 className='text-2xl font-bold shadow-xl'>Money Returns</h2>
+                    <p className="text-gray-300 text-sm">100% Money back guarntee</p>
                 </div>
             </div>
         </div>
--- src/HomeAllPage/Homepageallsection/Homepageallsection.jsx
+++ src/HomeAllPage/Homepageallsection/Homepageallsection.jsx
@@ -2,6 +2,7 @@ import Featuredprodut from "../../Component/FeaturedProduct/Featuredprodut";
 import Hero from "../../Component/Hero/Hero";
 import Benefitbuy from "../../Component/Benefitbuy/Benefitbuy";
 import Homepageslider from "../Homepageslider/Homepageslider";
+import Homepagehero from "../../Homepagehero/Homepagehero";
 
 function Homepageallsection() {
   
@@ -12,6 +13,7 @@ function Homepageallsection() {
       <Featuredprodut></Featuredprodut>
       <Homepageslider></Homepageslider>
       <Benefitbuy></Benefitbuy>
+      <Homepagehero></Homepagehero>
     </div>
   );
 }
